fd09634eeffae72af692b192a12dc4ea6f156811
Administrator <Administrator@zengwp>
2018-01-31 00:38:41 +0800

    pdf预览

--- jodconverter-web/src/main/java/cn/keking/service/impl/PdfFilePreviewImpl.java
+++ jodconverter-web/src/main/java/cn/keking/service/impl/PdfFilePreviewImpl.java
@@ -1,6 +1,8 @@
 package cn.keking.service.impl;
 
 import cn.keking.service.FilePreview;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -11,8 +13,12 @@ import org.springframework.ui.Model;
 @Service
 public class PdfFilePreviewImpl implements FilePreview{
 
+    @Value("${file.dir}")
+    String fileDir;
+    
     @Override
     public String filePreviewHandle(String url,String dbPath, Model model) {
+    	url=url.replace(fileDir,"");
         model.addAttribute("pdfUrl", url);
         return "pdf";
     }
